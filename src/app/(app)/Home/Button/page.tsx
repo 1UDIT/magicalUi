9ca59d1f979dd37b05316f7c379c26ac662dfa86
u121 <uditsharma7195@gmail.com>
2025-05-23 17:25:46 +0530

    test

--- src/app/(app)/Home/Button/page.tsx
+++ src/app/(app)/Home/Button/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 const GlowButton = () => {
-  const [activeTab, setActiveTab] = useState("account");
+  const [activeTab, setActiveTab] = useState("Preview");
   return (
     <>
       {/*<div className="relative w-full h-full overflow-hidden group items-center justify-center flex">
@@ -14,30 +14,30 @@ const GlowButton = () => {
         Sign up &rarr;
         <BottomGradient />
       </button> */}
-      <div className="w-[500px] h-[400px] mx-auto mt-10 bg-black text-white rounded-lg shadow-md p-4">
-        <div className="flex border-b border-gray-700 pb-7">
+      <div className="w-full h-[400px] mx-auto  dark:bg-black bg-white text-white rounded-lg shadow-md p-4 mt-10 border border-[#504f4f] p-2">
+        <div className="border-b border-dashed border-[#504f4f]">
           <button
-            onClick={() => setActiveTab("account")}
-            className={`flex-1 py-2 px-4 text-sm font-medium text-center rounded-t-md ${activeTab === "account"
+            onClick={() => setActiveTab("Preview")}
+            className={`py-2 px-4 text-sm font-medium text-center rounded-t-md ${activeTab === "Preview"
               ? "bg-gray-800 text-white"
               : "bg-gray-900 text-gray-400"
               }`}
           >
-            Account
+            Preview
           </button>
           <button
-            onClick={() => setActiveTab("password")}
-            className={`flex-1 py-2 px-4 text-sm font-medium text-center rounded-t-md ${activeTab === "password"
+            onClick={() => setActiveTab("Code")}
+            className={`py-2 px-4 text-sm font-medium text-center rounded-t-md ${activeTab === "Code"
               ? "bg-gray-800 text-white"
               : "bg-gray-900 text-gray-400"
               }`}
           >
-            Password
+            Code
           </button>
         </div>
 
-        <div className="p-4 border border-gray-700 border-t-0 rounded-b-md bg-gray-900 h-full w-full">
-          {activeTab === "account" ? (
+        <div className="border rounded-xl h-full w-full border-[#504f4f]">
+          {activeTab === "Preview" ? (
             <div className="relative w-full h-full overflow-hidden group items-center justify-center flex">
               <button
                 className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
