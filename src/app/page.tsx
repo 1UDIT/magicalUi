1c8d9b859f4f8cfc1f7ea0f9fa7e5bc9e4845d06
u121 <uditsharma7195@gmail.com>
2025-06-18 19:15:30 +0530

    btn added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Links from "@/Components/sideLink/SideBar";
 import Navbar from "@/Components/NavBar";
+import { Button } from "@/Components/UI/Button";
 
 
 
@@ -10,9 +11,18 @@ interface RootLayoutProps {
 export default async function RootLayout({ children }: RootLayoutProps) {
     return (
         <>
-            {/* <Links /> */}
-            <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+            <main className="flex-grow flex flex-col items-center justify-center px-4 text-white">
                 <section className="text-center mb-8 md:mb-12">
+                    <div className="w-full flex items-center justify-center">
+                        <div className="btn relative  cursor-pointer flex items-center justify-center rounded-[100px] border-none p-[2px] w-31 ">
+                            <p className="relative z-[1] w-full rounded-[100px] bg-black p-1 text-base text-[#fff] backdrop-blur-[40px]">
+                                Button
+                            </p>
+                        </div>
+                    </div>
+                    <div className="border-bar-button p-9">
+                        <span>En savoir +</span>
+                    </div> 
                     <h1 className="text-3xl md:text-5xl font-bold">
                         Dive into the World of Anonymous Feedback
                     </h1>
